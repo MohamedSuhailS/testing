36d2da3bcbe759d1790b2b9a976ef4fe7391fbbf
suhail <mohamed.s@smartladders.com>
2023-03-28 10:55:55 +0530

    make it better

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mysql = require('mysql');
-const app = express();
-var flash = require('express-flash');
-var bodyParser = require('body-parser');
-const cookieParser = require("cookie-parser");
-const session = require('express-session');
-var jsforce = require('jsforce');
-app.set('view engine', 'hbs');
-var path = require('path');
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
-app.use(cookieParser());
-dotenv.config({path: './.env'});
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-var db = new jsforce.Connection();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static((__dirname + 'public/css')));
-app.use('/image', express.static((__dirname + 'public/image')));
-db.login('syedsuhail926.ss@cunning-hawk-bhslhk.com', 'hack12345','m5FekhzLDBPwbSvXTbG1Y8Hp', function(err, res) {
-  if (err) { return console.error(err); }
-});
-app.use(cookieParser());
-app.use(session({ 
-    secret: '123456cat',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 1000000 }
-}))
-app.use('/', require('./routes/page'));
-app.use('/auth', require('./routes/auth'));
-app.use('/admin', require('./routes/adminauth'));
-app.use('/admin/admin', require('./routes/adminauth'));
+// var http = require("http")
+var axios = require('axios');
+// http.createServer(function (request, response) {
+//    // Send the HTTP header 
+//    // HTTP Status: 200 : OK
+//    // Content Type: text/plain
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+var config = {
+  method: 'get',
+maxBodyLength: Infinity,
+  url: 'https://dev131365.service-now.com/api/now/table/incident?sysparm_fields=number%2Cstate%2Cshort_description%2Ccategory%2Csys_created_by&sysparm_limit=100',
+  headers: {
+    'Authorization': 'Basic YWRtaW46QmdmUUt1dTUkViUy',
+    'Cookie': 'BIGipServerpool_dev131365=2844241674.64064.0000; JSESSIONID=D8E1385E5CE073F35519084E0888CAFE; glide_session_store=B54D9D04476121105ECC9DB6926D4358; glide_user_route=glide.be5997550394e44e56bec65b6fcc3ec5'
+  }
+};
 
-app.listen(process.env.PORT || 5000)
+
+
+//    // Send the response body as "Hello World"
+//    response.end('Hello World\n');
+// }).listen(8080);
+
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8080/');
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+const cors = require('cors')
+
+app.use(cors())
+app.get('/', (req, res) => {
+    axios(config)
+.then(function (response) {
+  res.send(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+})
+
+app.listen(port,
+  console.log(`Example app listening on port ${port}`));
